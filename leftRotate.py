80098d7299d3a553e89f9a87924757c1d27fe0d3
Khan Shayaan Shakeel <48132597+shayaankhan05@users.noreply.github.com>
2021-06-08 12:38:31 +0530

    Add files via upload

---
+++ leftRotate.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+#
+
+
+def rotLeft(a, d):
+    d = d % len(a)
+    a = a[d:]+a[:d]
+    return a
+
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    print(result)
