f30d09975ef75db28f072ca20dcebecf60c2a884
Khan Shayaan Shakeel <48132597+shayaankhan05@users.noreply.github.com>
2021-06-17 13:11:09 +0530

    Need to work on time complexity

---
+++ fradulent_activity.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import statistics
+
+#
+# Complete the 'activityNotifications' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY expenditure
+#  2. INTEGER d
+#
+
+
+def activityNotifications(expenditure, d):
+    notify = 0
+
+    lead = expenditure[d:]
+
+    for i in range(len(lead)):
+
+        med = statistics.median(expenditure[i:d+i])
+
+        if (lead[i] >= 2*med):
+
+            notify += 1
+
+    return notify
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    expenditure = list(map(int, input().rstrip().split()))
+
+    result = activityNotifications(expenditure, d)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
